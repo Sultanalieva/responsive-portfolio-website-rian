d9dbf992799bfb4b104b15948e79448005813e42
Sultanalieva <raih.sultanalieva@gmail.com>
2023-11-13 16:49:10 +0100

    Added about me section

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,13 @@ const linkAction = () => {
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== SHADOW HEADER ===============*/
-
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scroll >= 50
+    ? header - classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
 /*=============== EMAIL JS ===============*/
 
 /*=============== SHOW SCROLL UP ===============*/
